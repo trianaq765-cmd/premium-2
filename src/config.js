ba227bf29db7cba1763e19169969753503b6497c
trianaq765-cmd <trianaq765@gmail.com>
2026-01-03 20:52:31 +0700

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+// ============================================================
+// ⚙️ CONFIG
+// ============================================================
+
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    SCRIPT_SOURCE_URL: process.env.SCRIPT_SOURCE_URL || '',
+    ADMIN_KEY: process.env.ADMIN_KEY || 'change-this-to-secure-key'
+};
